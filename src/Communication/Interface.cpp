4789eed528ad3f1e4b1b27dd486de6e328c94b45
vertex <adam.makswiej@gmail.com>
2023-12-02 03:39:30 +0100

    refactor

--- src/Communication/Interface.cpp
+++ src/Communication/Interface.cpp
@@ -5,18 +5,10 @@
 #include "./Interface.hpp"
 #include <cstring>
 
-InterfaceData outputBuffer;
 auto buffer = "Testy test!";
 
 
 Communication::Interface::Interface() {
-    addCommand(new Lib::SPI::Slave::OutputCommand<InterfaceData>(1));
-    addCommand(new Lib::SPI::Slave::OutputCommand<uint16_t>(2));
-    addCommand(new Lib::SPI::Slave::InputCommand<uint32_t>(10, 5));
-    addCommand(new Lib::SPI::Slave::InputCommand<InterfaceData>(11));
-    addCommand(new Lib::SPI::Slave::InputCommand<uint32_t>(12, 64));
-    addCommand(new Lib::SPI::Slave::InputCommand<InterfaceData>(13, outputBuffer));
-
     _in_buf = new Lib::SPI::Slave::VariableInputBuffer();
     addCommand((new Lib::SPI::Slave::VarInputCommand(20, _in_buf))->checksum(false));
 
@@ -26,13 +18,6 @@ Communication::Interface::Interface() {
 
 void Communication::Interface::begin() {
     _out_buf->update(reinterpret_cast<Lib::SPI::Slave::Buffer::Data>(const_cast<char*>(buffer)), strlen(buffer));
-
-    outputBuffer.chr = 7;
-    outputBuffer.word = 8765;
-    outputBuffer.tf = true;
-    output()->update(&outputBuffer);
-
-    output2()->update(1111);
 
     setup();
 }
--- src/Communication/Interface.hpp
+++ src/Communication/Interface.hpp
@@ -2,13 +2,6 @@
 
 #include <vertaro/spi/slave.impl.hpp>
 
-
-typedef struct {
-    uint8_t chr;
-    uint16_t word;
-    bool tf;
-} __attribute__ ((aligned(1), packed)) InterfaceData;
-
 namespace Communication {
 class Interface final : protected Lib::SPI::Slave::Interface {
 protected:
@@ -19,22 +12,6 @@ public:
     Interface();
 
     void begin();
-
-    Lib::SPI::Slave::OutputCommand<InterfaceData> *output() {
-        return static_cast<Lib::SPI::Slave::OutputCommand<InterfaceData> *>(_commands[0]);
-    }
-
-    Lib::SPI::Slave::OutputCommand<uint16_t> *output2() {
-        return static_cast<Lib::SPI::Slave::OutputCommand<uint16_t> *>(_commands[1]);
-    }
-
-    Lib::SPI::Slave::InputCommand<InterfaceData> *input() {
-        return static_cast<Lib::SPI::Slave::InputCommand<InterfaceData> *>(_commands[2]);
-    }
-
-    Lib::SPI::Slave::InputCommand<uint32_t> *input2() {
-        return static_cast<Lib::SPI::Slave::InputCommand<uint32_t> *>(_commands[3]);
-    }
 
     Lib::SPI::Slave::VariableInputBuffer *inBuf() {
         return _in_buf;
