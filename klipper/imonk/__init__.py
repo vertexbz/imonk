ed2e88b2e9b78b0b839c9bd5dd3f463c925a6a7e
vertex <adam.makswiej@gmail.com>
2023-12-06 01:15:55 +0100

    initial write

--- klipper/imonk/__init__.py
+++ klipper/imonk/__init__.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 from typing import TYPE_CHECKING
 from .spi import SPI
+from .spi.error import CommunicationError
 
 if TYPE_CHECKING:
     from gcode import GCodeCommand
@@ -15,14 +16,32 @@ class IMONK:
         self.gcode: GCodeDispatch = self.printer.lookup_object('gcode')
         self.spi = SPI(config)
 
-        self.gcode.register_command('IMONK_VERSION', self.cmd_IMONK_VERSION, desc=self.cmd_IMONK_VERSION_help)
-
-    cmd_IMONK_VERSION_help = "Print current IMONK firmware version"
+        self.gcode.register_command('IMONK_VERSION', self.cmd_IMONK_VERSION, desc="Print current IMONK firmware version")
+        self.gcode.register_command('IMONK_TEST', self.cmd_IMONK_TEST, desc="Test")
+        self.gcode.register_command('IMONK_TEST2', self.cmd_IMONK_TEST2, desc="Test2")
 
     def cmd_IMONK_VERSION(self, gcmd: GCodeCommand):
         with self.spi as spi:
-            response = spi.read_command(0x01, 3)
-            gcmd.respond_info(f'IMONK Firmware Version {response[0]}.{response[1]}.{response[2]}')
+            try:
+                version = spi.read_command(0x01, 3)
+                gcmd.respond_info(f'IMONK Firmware Version {version[0]}.{version[1]}.{version[2]}')
+            except CommunicationError as e:
+                raise gcmd.error(str(e))
+
+    def cmd_IMONK_TEST(self, gcmd: GCodeCommand):
+        with self.spi as spi:
+            try:
+                response = spi.read_command(0xA1).decode('ascii')
+                gcmd.respond_info(f'Answer: {response}')
+            except CommunicationError as e:
+                raise gcmd.error(str(e))
+
+    def cmd_IMONK_TEST2(self, gcmd: GCodeCommand):
+        with self.spi as spi:
+            try:
+                spi.write_command(0xA0, gcmd.get('TEXT', 'TEST123').encode('ascii'))
+            except CommunicationError as e:
+                raise gcmd.error(str(e))
 
 
 def load_config(config: ConfigWrapper):
--- klipper/imonk/spi/transaction.py
+++ klipper/imonk/spi/transaction.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 from typing import Optional
 from extras import bus
 from .crc import crc16_quick
-from .error import CRCError
+from .error import CRCError, CommunicationError
 
 
 class SPITransaction:
@@ -13,19 +13,50 @@ class SPITransaction:
         params = self._spi.spi_transfer([val])
         return params['response']
 
-    def read_crc(self) -> int:
-        return int.from_bytes(self.transfer(0x00) + self.transfer(0x00), "little")
+    def read_word(self) -> int:
+        return int.from_bytes(self.transfer(0x00) + self.transfer(0x00), 'little')
 
     def read_command(self, cmd: int, length: Optional[int] = None) -> bytes:
         buf = bytearray()
+        crc_buf = bytearray()
 
         self.transfer(cmd)
+        if length is None:
+            length = self.read_word()
+            crc_buf.extend(length.to_bytes(2, 'little'))
+
         for i in range(length):
             buf.extend(self.transfer(0x00))
-        crc = self.read_crc()
 
-        crc_computed = crc16_quick(buf)
+        # CRC
+        crc = self.read_word()
+        crc_buf.extend(buf)
+        crc_computed = crc16_quick(crc_buf)
         if crc != crc_computed:
             raise CRCError(crc, crc_computed)
 
         return bytes(buf)
+
+    def duplex_write(self, val: int, last: int) -> None:
+        response = self.transfer(val)[0]
+        if response != last:
+            raise CommunicationError(f'Expected echo of last byte {hex(last)[2:].upper()}, but received {hex(response)[2:].upper()}')
+
+    def write_command(self, cmd: int, data: bytes, crc: bool = False) -> None:  # TODO CRC
+        self.transfer(cmd)
+
+        last = 0
+        for b in len(data).to_bytes(2, 'little'):
+            self.duplex_write(b, last)
+            last = b
+
+        for b in data:
+            self.duplex_write(b, last)
+            last = b
+
+        self.duplex_write(0x00, last)
+
+        result = self.transfer(0x00)[0]
+        if result != 0xA5:
+            raise CommunicationError('Write failed')
+
