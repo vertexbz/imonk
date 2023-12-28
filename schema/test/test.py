c011a87c99730aa84883f0c743caea2687f9412a
vertex <adam.makswiej@gmail.com>
2023-12-28 22:21:11 +0100

    schema updates

--- schema/test/test.py
+++ schema/test/test.py
@@ -1,13 +1,13 @@
 import json
 from difflib import ndiff
-from config import Scene
+from config import View
 
 if __name__ == "__main__":
-    for n in range(3):
+    for n in range(5):
         with open(f'test{n+1}.json', 'r', encoding='ascii') as f:
             data = json.load(f)
             data_str = json.dumps(data)
-            scene = Scene(**data)
+            scene = View(**data)
 
             print(scene)
             print('\n'.join(ndiff([data_str], [json.dumps(scene.dump())])))
